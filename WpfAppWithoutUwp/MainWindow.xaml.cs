f889e5a0e90ce4563d543505968614f4ba9b26b6
Daniels Danilins <nesher_hh@msn.com>
2021-05-03 17:24:50 +0200

    added map control

--- WpfAppWithoutUwp/MainWindow.xaml.cs
+++ WpfAppWithoutUwp/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Windows;
 using Microsoft.Toolkit.Wpf.UI.XamlHost;
+using windows = Windows;
+
 
 namespace WpfAppWithoutUwp
 {
@@ -22,5 +25,38 @@ namespace WpfAppWithoutUwp
 					break;
 			}
 		}
+
+		private async void myMap_Loaded(object sender, RoutedEventArgs e)
+		{
+			if (myMap.Layers.Count == 0)
+			{
+				// Specify a known location.
+				var cityPosition = new windows.Devices.Geolocation.BasicGeoposition()
+				{
+					Latitude = 47.604,
+					Longitude = -122.329
+				};
+				var cityCenter = new windows.Devices.Geolocation.Geopoint(cityPosition);
+				var icon = new windows.UI.Xaml.Controls.Maps.MapIcon()
+				{
+					Location = cityCenter,
+				};
+
+				var elements = new System.Collections.Generic.List<windows.UI.Xaml.Controls.Maps.MapElement>()
+				{
+					icon,
+				};
+				var layer = new windows.UI.Xaml.Controls.Maps.MapElementsLayer()
+				{
+					ZIndex = 1,
+					MapElements = elements,
+				};
+				myMap.Layers.Add(layer);
+
+				// Set the map location.
+				await myMap.TrySetViewAsync(cityCenter, 12).ConfigureAwait(false);
+			}
+		}
+
 	}
 }
