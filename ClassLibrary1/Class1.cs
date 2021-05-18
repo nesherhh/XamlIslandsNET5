da71081c804806b8118fda2da5a31bc24bd1f5c5
Daniels Danilins <nesher_hh@msn.com>
2021-05-18 15:21:39 +0200

    added .NET 5 WinRT project with custom control

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+	public sealed class Class1
+	{
+	}
+}
---
+++ ClassLibrary1/Controls/HudControl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+
+namespace ClassLibrary1.Controls
+{
+	public sealed class HudControl : Control
+	{
+		public HudControl()
+		{
+			DefaultStyleKey = typeof(HudControl);
+		}
+	}
+}
